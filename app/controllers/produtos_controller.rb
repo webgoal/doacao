34e519c0559216e0277b41f69ff4b597ea45a671
Alain Tramont <alain@webgoal.com.br>
2017-06-25 11:03:49 -0300

    Funcao new

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -2,6 +2,9 @@ class ProdutosController < ApplicationController
   def index
   end
   def create
-    @produto = Produtos.new(params[:pro])
+    @produto = Produtos.new(params[:produtos])
+
+    @produto.save
+    redirect_to @produto
   end
 end
